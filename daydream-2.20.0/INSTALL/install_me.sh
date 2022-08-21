2f4b674aa1b50b122d2b21d52e1e48ef30dcea26
Jan Gyldenlykke <ia2115@yahoo.com>
2022-08-21 03:42:34 +0000

    Updated INSTALL/install_me.sh script to support FreeBSD syntax

--- daydream-2.20.0/INSTALL/install_me.sh
+++ daydream-2.20.0/INSTALL/install_me.sh
@@ -5,16 +5,27 @@ then
   echo "Run sh install_me.sh <path-to-install>"
 fi
 
+platform='unknown'
+unamestr=$(uname)
+if [ "$unamestr" = 'Linux' ]; then
+   platform='linux'
+elif [ "$unamestr" = 'FreeBSD' ]; then
+   platform='freebsd'
+fi
+
 INSTALL_PATH=$1
 
-if [ -n `echo $INSTALL_PATH|grep -e "/$"` ];
+if [ -n `echo $INSTALL_PATH|grep -e "/$"` ];   # check if $INSTALL_PATH is empty
 then
   INSTALL_PATH=`echo $INSTALL_PATH| sed "s/\/$//"`
 fi
-
 mkdir -p $INSTALL_PATH
 
-cp -r batch/ $INSTALL_PATH
+if [ "$platform" = 'linux' ]; then
+  cp -r batch/ $INSTALL_PATH
+elif [ "$platform" = 'freebsd' ]; then
+  cp -R batch $INSTALL_PATH
+fi
 
 X="2 3 10 11 12 13 14"
 for i in $X
@@ -22,39 +33,68 @@ do
   ln -f -s $INSTALL_PATH/batch/batch1.logoff $INSTALL_PATH/batch/batch$i.logoff
 done
 
-# bbs dirs
-mkdir $INSTALL_PATH/bin
-cp -r bulletins/ $INSTALL_PATH
-mkdir $INSTALL_PATH/configs
-mkdir $INSTALL_PATH/configs/ddtop_designs
-mkdir $INSTALL_PATH/confs
-cp -r data/ $INSTALL_PATH
-cp -r display/ $INSTALL_PATH
-mkdir $INSTALL_PATH/doors
-mkdir $INSTALL_PATH/lib
-mkdir $INSTALL_PATH/logfiles
-cp -r python/ $INSTALL_PATH
-cp -r questionnaire/ $INSTALL_PATH
-cp -r scripts/ $INSTALL_PATH
-mkdir $INSTALL_PATH/temp
-mkdir $INSTALL_PATH/users
-mkdir $INSTALL_PATH/utils
-# fido dirs
-mkdir $INSTALL_PATH/fido
-mkdir $INSTALL_PATH/fido/in
-mkdir $INSTALL_PATH/fido/in.sec
-mkdir $INSTALL_PATH/fido/in.loc
-mkdir $INSTALL_PATH/fido/out
-mkdir $INSTALL_PATH/fido/bad
-mkdir $INSTALL_PATH/fido/lists
-mkdir $INSTALL_PATH/fido/announce
+if [ "$platform" = 'linux' ]; then
+  # bbs dirs
+  mkdir $INSTALL_PATH/bin
+  cp -r bulletins/ $INSTALL_PATH
+  mkdir $INSTALL_PATH/configs
+  mkdir $INSTALL_PATH/configs/ddtop_designs
+  mkdir $INSTALL_PATH/confs
+  cp -r data/ $INSTALL_PATH
+  cp -r display/ $INSTALL_PATH
+  mkdir $INSTALL_PATH/doors
+  mkdir $INSTALL_PATH/lib
+  mkdir $INSTALL_PATH/logfiles
+  cp -r python/ $INSTALL_PATH
+  cp -r questionnaire/ $INSTALL_PATH
+  cp -r scripts/ $INSTALL_PATH
+  mkdir $INSTALL_PATH/temp
+  mkdir $INSTALL_PATH/users
+  mkdir $INSTALL_PATH/utils
+  # fido dirs
+  mkdir $INSTALL_PATH/fido
+  mkdir $INSTALL_PATH/fido/in
+  mkdir $INSTALL_PATH/fido/in.sec
+  mkdir $INSTALL_PATH/fido/in.loc
+  mkdir $INSTALL_PATH/fido/out
+  mkdir $INSTALL_PATH/fido/bad
+  mkdir $INSTALL_PATH/fido/lists
+  mkdir $INSTALL_PATH/fido/announce
+elif [ "$platform" = 'freebsd' ]; then
+  # bbs dirs
+  mkdir $INSTALL_PATH/bin
+  cp -R bulletins $INSTALL_PATH
+  mkdir $INSTALL_PATH/configs
+  mkdir $INSTALL_PATH/configs/ddtop_designs
+  mkdir $INSTALL_PATH/confs
+  cp -R data $INSTALL_PATH
+  cp -R display $INSTALL_PATH
+  mkdir $INSTALL_PATH/doors
+  mkdir $INSTALL_PATH/lib
+  mkdir $INSTALL_PATH/logfiles
+  cp -R python $INSTALL_PATH
+  cp -R questionnaire $INSTALL_PATH
+  cp -R scripts $INSTALL_PATH
+  mkdir $INSTALL_PATH/temp
+  mkdir $INSTALL_PATH/users
+  mkdir $INSTALL_PATH/utils
+  # fido dirs
+  mkdir $INSTALL_PATH/fido
+  mkdir $INSTALL_PATH/fido/in
+  mkdir $INSTALL_PATH/fido/in.sec
+  mkdir $INSTALL_PATH/fido/in.loc
+  mkdir $INSTALL_PATH/fido/out
+  mkdir $INSTALL_PATH/fido/bad
+  mkdir $INSTALL_PATH/fido/lists
+  mkdir $INSTALL_PATH/fido/announce
+fi
 
 SED_EXPR_FROM=`echo "/home/bbs"|sed 's/\\//\\\\\\//g'`
 SED_EXPR_TO=`echo "$INSTALL_PATH"|sed 's/\\//\\\\\\//g'`
 
 for i in `find configs/ -type f|xargs`
 do
-  cat $i| sed --posix -e "s/$SED_EXPR_FROM/$SED_EXPR_TO/g" > $INSTALL_PATH/$i
+  cat $i| sed -e "s/$SED_EXPR_FROM/$SED_EXPR_TO/g" > $INSTALL_PATH/$i
 done
 
 echo "export LD_LIBRARY_PATH=$INSTALL_PATH/lib" > $INSTALL_PATH/scripts/ddenv.sh
